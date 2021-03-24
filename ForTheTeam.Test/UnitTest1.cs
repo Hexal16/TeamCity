bce0f643d6a48e716a7ba0f93bea8bc83ee2fcd9
Gerda <gerdaditeriha@gmail.com>
2021-03-24 19:33:19 +0200

    Just some changes

--- ForTheTeam.Test/UnitTest1.cs
+++ ForTheTeam.Test/UnitTest1.cs
@@ -17,5 +17,14 @@ namespace ForTheTeam.Test
             Assert.That(result.Item1, Is.EqualTo(2019));
             Assert.That(result.Item2, Is.Empty);
         }
+        
+        [Test]
+        public void GetYear_Test1()
+        {
+            var test = new Program();
+            var result  = test.GetYear("2020");
+            Assert.That(result.Item1, Is.EqualTo(2020));
+            Assert.That(result.Item2, Is.Empty);
+        }
     }
 }
