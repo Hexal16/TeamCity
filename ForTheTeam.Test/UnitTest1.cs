9e190244094af7fb46f014c909c0234374ee5528
Gerda <gerdaditeriha@gmail.com>
2021-03-24 19:43:57 +0200

    Wrong test added

--- ForTheTeam.Test/UnitTest1.cs
+++ ForTheTeam.Test/UnitTest1.cs
@@ -26,5 +26,14 @@ namespace ForTheTeam.Test
             Assert.That(result.Item1, Is.EqualTo(2020));
             Assert.That(result.Item2, Is.Empty);
         }
+        
+        [Test]
+        public void GetYear_Test2()
+        {
+            var test = new Program();
+            var result  = test.GetYear("2020");
+            Assert.That(result.Item1, Is.EqualTo(1000));
+            Assert.That(result.Item2, Is.Empty);
+        }
     }
 }
